e76fc1d1657321f31d8d24209f3baec710e4d100
rafael331020-crypto <rafael331020@gmail.com>
2026-09-02 16:06:07 -0300

    Configure LabManager app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.rafael.labmanager"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.rafael.labmanager"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    kotlin { jvmToolchain(17) }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+}
