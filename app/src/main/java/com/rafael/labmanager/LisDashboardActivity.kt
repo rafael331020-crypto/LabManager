697b90f55c0a2fed03c906b2a6f580344a2191cc
rafael331020-crypto <rafael331020@gmail.com>
2026-09-02 20:05:22 -0300

    fix: import View for LIS dashboard build

--- app/src/main/java/com/rafael/labmanager/LisDashboardActivity.kt
+++ app/src/main/java/com/rafael/labmanager/LisDashboardActivity.kt
@@ -5,6 +5,7 @@ import android.graphics.Color
 import android.graphics.Typeface
 import android.os.Bundle
 import android.view.Gravity
+import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
