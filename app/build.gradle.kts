80f5343f29659221071d4a8eb52ae366034f5539
rafael331020-crypto <rafael331020@gmail.com>
2026-09-02 19:49:44 -0300

    chore: bump LabManager LIS to version 2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.rafael.labmanager"
         minSdk = 23
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
     }
 
     kotlin { jvmToolchain(17) }
